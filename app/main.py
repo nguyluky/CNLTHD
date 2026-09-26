af41f46f305e1bcd8dfe64221d4b0cc53f9bd425
VanChi <chi876543@gmail.com>
2026-09-26 15:25:56 +0700

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from redis_fastapi import FastAPIRedis
 
 from app.core.exception import ErrorModel, ValidationErrorModel
@@ -26,6 +27,11 @@ async def lifespan(app: FastAPI):
         await engine.dispose()
 
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
 app = FastAPI(title=APP_NAME, version="1.0.0", lifespan=lifespan, responses={
     500: {
         "model": ErrorModel,
@@ -37,6 +43,15 @@ app = FastAPI(title=APP_NAME, version="1.0.0", lifespan=lifespan, responses={
     },
 })
 FastAPIRedis(app).lifespan()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user.router)
 # app.include_router(test.router)
